2a5c27d72facb0b006417df4bb325f3d2a6ea6a0
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-09-25 00:32:34 +0200

    Juste un peu d'aide

--- code.js
+++ code.js
@@ -12,12 +12,14 @@
 
 
 
-
-
-
-
 // ---------------------------
 
+
+const fs = require("fs");
+
+const wate = process.argv[1].substring(process.argv[1].lastIndexOf("\\")+1),
+	  filename = process.argv[2],
+	  func = process.argv[3];
 
 function exists(str) {
 	try {
@@ -45,7 +47,6 @@ String.prototype.replaceArrayWithString = function(arr, str) {
 	return replaced;
 };
 
-const fs = require("fs");
 
 String.prototype.replaceArrayWithArray = function(arr, omg) {
 	var replaced = this;
@@ -57,23 +58,19 @@ String.prototype.replaceArrayWithArray = function(arr, omg) {
 	return replaced;
 };
 
-const wate = process.argv[1].substring(process.argv[1].lastIndexOf("\\")+1),
-	  filename = process.argv[2],
-	  func = process.argv[3];
 
-if (process.argv.length < 3 || process.argv.some(pd => !pd)) return console.log('\x1b[31mUtilisation : node '+wate+' "fichier.js" ou node '+wate+' 2 "0x1324(0x987)"\x1b[0m'), process.exit();
-if (process.argv[2] == 2) return console.log(eval(func)), process.exit();
+if (process.argv.length < 3 || process.argv.some(pd => !pd)) return console.log(`\x1b[31mUtilisation : node ${wate} "fichier.js" ou node ${wate} 2 "0x1324(0x987)"\x1b[0m`), process.exit();
+if (filename == 2) return console.log(eval(func)), process.exit();
 
-let trans = wate.substring(0, wate.lastIndexOf(".")), gay = filename.substring((58454+565/10)*0, filename.lastIndexOf("."));
+let pasTrans = wate.substring(0, wate.lastIndexOf(".")), pasGay = filename.substring((58454+565/10)*0, filename.lastIndexOf("."));
 
-if (wate === filename || wate === gay || (trans && trans === filename) || (trans && gay && trans === gay)) return console.log("\x1b[31mVeuillez ne pas désobfusquer le fichier actuel, cela pourrait causer des problèmes. (créez-en plutôt un nouveau)\x1b[0m"), process.exit();
-if (!fs.existsSync(filename)) return console.log("\x1b[31mLe fichier "+filename+" n'existe pas.\x1b[0m"), process.exit();
+if (wate === filename || wate === pasGay || (pasTrans && pasTrans === filename) || (pasTrans && pasGay && pasTrans === pasGay)) return console.log("\x1b[31mVeuillez ne pas désobfusquer le fichier actuel, cela pourrait causer des problèmes. (créez-en plutôt un nouveau)\x1b[0m"), process.exit();
+if (!fs.existsSync(filename)) return console.log(`\x1b[31mLe fichier ${filename} n'existe pas.\x1b[0m`), process.exit();
 
 try {
 	fs.readFile(filename, (err, buf) => {
 		if (err) throw err;
 		let data = buf.toString(),
-			slash = "\\",
 			obf0 = data.match(RegExp("_0x\\w*\\([^)]*.","g")) || [];
 		
 		obf0 = obf0.conserveOne();
@@ -81,25 +78,25 @@ try {
 		let obf = [],
 			deob = [];
 		
-		obf0.forEach(d4rk => {
-			if (exists(d4rk)) obf.push(d4rk);
-			if (exists(d4rk.substring(d4rk.indexOf("(")+1))) obf.push(d4rk.substring(d4rk.indexOf("(")+1));
+		obf0.forEach(d4rkTmtc => {
+			if (exists(d4rkTmtc)) obf.push(d4rkTmtc);
+			if (exists(d4rkTmtc.substring(d4rkTmtc.indexOf("(")+1))) obf.push(d4rkTmtc.substring(d4rkTmtc.indexOf("(")+1));
 		});
 		
-		obf.forEach(gay=>deob.push(eval(gay)));
+		obf.forEach(pasGay=>deob.push(eval(pasGay)));
 		
-		let deobfuscated = data.replaceArrayWithArray(obf, deob).replace(/\\x[\w-]{2}/g, function (value) {
-				let newVal = eval(`"${value}"`);
-				if (value === "\\x27" || value === "\\x22") newVal = "\\"+newVal;
-				if (value === "\\x0A" || value === "\\x0a") newVal = "\\n";
-				if (value === "\\x1B" || value === "\\x1b") newVal = "\\x1b";
-				if (value === "\\x5C" || value === "\\x5c") newVal = "\\";
+		let deobfuscated = data.replaceArrayWithArray(obf, deob).replace(/\\x[\w-]{2}/g, function (val) {
+				let newVal = eval(`"${val}"`);
+				if (val === "\\x27" || val === "\\x22") newVal = "\\"+newVal;
+				if (val.toLowerCase() === "\\x0a") newVal = "\\n";
+				if (val.toLowerCase() === "\\x1b") newVal = "\\x1b";
+				if (val.toLowerCase() === "\\x5c") newVal = "\\";
 				return newVal;
-			}).replace(/\\u[\w-]{4}/g, function (value) {
-				let newVal = eval(`"${value}"`);
-				if (value === "\\u0027" || value === "\u0022") newVal = "\\"+newVal;
-				if (value === "\\u000A" || value === "\\u000a") newVal = "\\n", console.log(value);
-				if (value === "\\u005C" || value === "\\u005c") newVal = "\\", console.log(value);
+			}).replace(/\\u[\w-]{4}/g, function (val) {
+				let newVal = eval(`"${val}"`);
+				if (val === "\\u0027" || val === "\\u0022") newVal = "\\"+newVal;
+				if (val.toLowerCase() === "\\u000a") newVal = "\\n", console.log(val);
+				if (val.toLowerCase() === "\\u005c") newVal = "\\", console.log(val);
 				return newVal;
 			});
 		
@@ -107,7 +104,7 @@ try {
 		
 		fs.writeFile(filename, newData, (er) => {
 			if (er) throw er;
-			console.log("\x1b[32mLe fichier "+filename+" a correctement été désobfusqué\x1b[0m");
+			console.log(`\x1b[32mLe fichier ${filename} a correctement été désobfusqué\x1b[0m`);
 			process.exit();
 		});
 	});
